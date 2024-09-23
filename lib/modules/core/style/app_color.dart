3447d0476ca4ba0b02e87ec34091c23e64c8f09e
Renan Pereira Volpe <renanvolpe067@gmail.com>
2024-09-23 16:28:29 -0300

    feat: update appcolor

--- lib/modules/core/style/app_color.dart
+++ lib/modules/core/style/app_color.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppColor {
-  static const Color kPrimary = Color(0xFF2E004E);
-  static const Color kPrimaryLight = Color(0xFF8A20D5);
-  static const Color kBackground = Color(0xFFE0B6FF);
-  static const Color kBackgroundLight = Color.fromARGB(69, 208, 188, 255);
-  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kPrimary = Color(0xFF1E4DC5);
+  static const Color kPrimaryLight = Color(0xFF61A4E6);
+  static const Color kBlackText = Color(0xFF212121);
   static const Color kBlack = Color(0xFF000000);
-  static const Color kBlackText = Color(0xFF000000);
+  static const Color kWhite = Color(0xFFFFFFFF);
+  static const Color kBackgroundLight = Color(0xFFF5F5F5);
+  static const Color kBackground = Color(0xFFE8E8E8);
+
+  static const Color kPrimaryDark = Color(0xFF102E5C);
+  static const Color kBlackTextDark = Color(0xFFBDBDBD);
+  static const Color kBackgroundDark = Color(0xFF282828);
+  static const Color kAccentDark = Color(0xFF6B87B5);
 
   static const Color kAccent = Color(0xFFF50057);
   static const Color grey1 = Color(0xFF333333);
